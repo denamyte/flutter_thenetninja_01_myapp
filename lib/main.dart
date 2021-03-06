a1ffc1d5ca6527629478e4b3acbcc8683e4bec65
denamyte <denamyte@gmail.com>
2021-03-06 02:27:03 +0200

    Flutter Tutorial for Beginners #5 - Scaffold & AppBar Widgets(https://www.youtube.com/watch?v=C5lpPjoivaw)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('hey ninjas!'),
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('my first app'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text('hello ninjas!'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {  },
+      child: Text('click'),
+    ),
+  ),
 ));
 
 
