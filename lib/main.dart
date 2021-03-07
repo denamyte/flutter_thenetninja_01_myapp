29556a9f3a7fa600babe0af110d26adeb22af0fd
denamyte <denamyte@gmail.com>
2021-03-08 01:42:00 +0200

    Flutter Tutorial for Beginners #6 - Colours & Fonts(https://www.youtube.com/watch?v=km2P_KQJyO0)

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,32 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   home: Scaffold(
     appBar: AppBar(
-      title: Text('my first app'),
+      title: Text(
+        'my first app',
+        style: TextStyle(
+          fontSize: 25,
+          fontFamily: 'AkayaTelivigala',
+        ),
+      ),
       centerTitle: true,
+      backgroundColor: Colors.red[600],
     ),
     body: Center(
-      child: Text('hello ninjas!'),
+      child: Text(
+          'hello ninjas!',
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2,
+          color: Colors.grey[600],
+          fontFamily: 'IndieFlower',
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
-      onPressed: () {  },
+      onPressed: () {},
       child: Text('click'),
+      backgroundColor: Colors.red,
     ),
   ),
 ));
