0c11482dec893e467b82cb1a6a8e9e4634fc8a53
denamyte <denamyte@gmail.com>
2021-03-10 01:03:21 +0200

    Flutter Tutorial for Beginners #8 - Images & Assets(https://www.youtube.com/watch?v=Hxh6nNHSUjo)

--- lib/main.dart
+++ lib/main.dart
@@ -20,16 +20,12 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          'hello ninjas!',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower',
-          ),
-        ),
+        // child: Image(
+          // image: NetworkImage('https://images.unsplash.com/photo-1615199905555-1dc4f184621a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1241&q=80'),
+          // image: AssetImage('assets/images/pic-3.jpg'),
+        // ),
+        // child: Image.network('a url from the Internet'),
+        child: Image.asset('assets/images/pic-3.jpg'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
@@ -40,5 +36,17 @@ class Home extends StatelessWidget {
   }
 }
 
+/*
 
+Text(
+          'hello ninjas!',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
+        )
+ */
 
