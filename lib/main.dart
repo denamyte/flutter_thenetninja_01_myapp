88127881b8950ab9909664b84b8b7c1f2b35a5d1
denamyte <denamyte@gmail.com>
2021-03-10 00:25:59 +0200

    Flutter Tutorial for Beginners #7 - Stateless Widgets & Hot Reload(https://www.youtube.com/watch?v=zwPBMg3SHVU)

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text(
-        'my first app',
-        style: TextStyle(
-          fontSize: 25,
-          fontFamily: 'AkayaTelivigala',
-        ),
-      ),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
-          'hello ninjas!',
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2,
-          color: Colors.grey[600],
-          fontFamily: 'IndieFlower',
-        ),
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text('click'),
-      backgroundColor: Colors.red,
-    ),
-  ),
+  home: Home(),
 ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'my first app',
+          style: TextStyle(
+            fontSize: 25,
+            fontFamily: 'AkayaTelivigala',
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          'hello ninjas!',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
+
 
 
