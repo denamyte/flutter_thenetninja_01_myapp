0a8d5afff16641b08a8fcc2c43fbfecb7d047f82
denamyte <denamyte@gmail.com>
2021-03-14 02:19:07 +0200

    Lesson 11. Basic Row

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,23 @@ class Home extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.red[600]
       ),
-      body: ,
+      body: Row(
+        children: <Widget>[
+          Text('Hello, World!'),
+          TextButton(
+            onPressed: () {},
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.amber)
+            ),
+            child: Text('Click me!'),
+          ),
+          Container(
+            color: Colors.cyan,
+            padding: EdgeInsets.all(30.0),
+            child: Text('Inside the container'),
+          )
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.red[600],
         child: Text('click'),
